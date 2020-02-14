e35ebc60eef2b32230ade9930612b8654aa0954c
Tatas Fachrul <tatasfachrul@gmail.com>
2020-02-14 14:13:44 +0700

    CRUD Book

--- index.js
+++ index.js
@@ -22,7 +22,18 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (request, response) => {
-  connection.query('SELECT * FROM book', (error, result) => {
+  const searchName = request.query.name || ''
+  connection.query(`SELECT * FROM book WHERE name LIKE '%${searchName}%'`, (error, result) => {
+    if (error) console.log(error)
+    response.json(result)
+  })
+})
+
+app.get('/:bookId', (request, response) => {
+  const bookId = request.params.bookId
+  console.log(bookId)
+
+  connection.query('SELECT * FROM book WHERE id = ?', bookId, (error, result) => {
     if (error) console.log(error)
     response.json(result)
   })
@@ -41,6 +52,34 @@ app.post('/', (request, response) => {
     updated_at: new Date()
   }
   connection.query('INSERT INTO book SET ?', data, (error, result) => {
+    if (error) console.log(error)
+    response.json(result)
+  })
+})
+
+app.patch('/:bookId', (request, response) => {
+  const bookId = request.params.bookId
+
+  const data = {
+    name: request.body.name,
+    writer: request.body.writer,
+    description: request.body.description,
+    publisher: request.body.publisher,
+    year: request.body.year,
+    stock: request.body.stock,
+    genre: request.body.genre,
+    updated_at: new Date()
+  }
+
+  connection.query('UPDATE book SET ? WHERE id = ?', [data, bookId], (error, result) => {
+    if (error) console.log(error)
+    response.json(result)
+  })
+})
+
+app.delete('/:bookId', (request, response) => {
+  const bookId = request.params.bookId
+  connection.query('DELETE FROM book WHERE id = ?', bookId, (error, result) => {
     if (error) console.log(error)
     response.json(result)
   })
