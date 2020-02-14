24ebf630e886a5cad008a551c4f1e21344d78aab
Tatas Fachrul <tatasfachrul@gmail.com>
2020-02-14 10:45:05 +0700

    first method get and post

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('This is nodemon')
+const express = require('express')
+const app = express()
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
+app.listen(8001, () => console.log('\n This server is running'))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (request, response) => {
+  response.json({name: 'Tatas Fachrul'})
+})
+
+app.post('/', (request, response) => {
+  const name = request.body.name
+
+  response.json({name})
+})
